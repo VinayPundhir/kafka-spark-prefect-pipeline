f088fc5a43c14b5636dff4b654ea0d6cce1a6f68
Vinay-Pundhir <vinay.pundhir@vamstar.io>
2023-07-23 20:45:54 +0530

    refactor code . add readme

--- database/sqlite.py
+++ database/sqlite.py
@@ -4,7 +4,7 @@ from config import DB_NAME
 
 
 def create_table(query: str):
-    """sqlite implementation to create new table id does not exists"""
+    """sqlite implementation to create new table if it does not exist"""
     try:
         conn = sqlite3.connect(DB_NAME)
         cursor = conn.cursor()
